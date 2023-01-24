247b4f202753c04a4702de357e359e68a92be48b
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-01-24 16:21:09 -0700

    added counter

--- js/app.js
+++ js/app.js
@@ -18,29 +18,42 @@ console.log(Tamagotchi = new Pet (petName, 10, 10, 10, 1))
 //Create event listeners/handlers that call methods in your game class
 //Display a charecter to represent your pet, must move
 
-
 //Hunger (1-10 scale), Sleepiness (1-10 scale), Boredom (1-10 scale), Age Up
     //Make these decrease on intervals and increase with user action
 
-// let hunger =
-//     document.getElementById("#hunger");
-//     console.log(hunger)
-// let sleepLife = 
-//     document.getElementById("#energy");
-//     console.log(energy)
-// let playLife =
-//     document.getElementById("#play");
-//     console.log(play)
+let hunger =
+    document.getElementById("#hunger");
+    
+let energy = 
+    document.getElementById("#energy");
+    
+let play =
+    document.getElementById("#play");
+ 
 // function hungerLife(){
 //     for (i = 10, i > 0, i--)
 
 // }  
     
 //Add buttons to feed your pet, turn off the lights, and play with your pet
-    const hungerBtn = document.querySelector("#action-Feed");
-    const sleepBtn = document.querySelector("#action-Sleep");
-    const playBtn = document.querySelector("#action-Play");
-    const lightBtn = document.querySelector("#action-Light");
+
+    button.addEventListener("click", Feed());
+    function Feed() {
+    for(let h = 0; h >= 10; h++);{
+       return (hunger[h] + 1) 
+    }
+
+    }
+    button.addEventListener("click", Energy());
+
+    button.addEventListener("click", Play());
+
+    button.addEventListener("click", Lights());
+    
+function lights() {
+        document.getElementById("lights");
+    }
+
 //Add scores for each button
 
     // Make buttons work
