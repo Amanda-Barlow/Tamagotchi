de7a8f16313b006bfe1094e8212f4cb06f0dbbec
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-01-24 21:08:12 -0700

    moved JS to JS file and fixed CSS

--- js/app.js
+++ js/app.js
@@ -30,21 +30,47 @@ let energy =
 let play =
     document.getElementById("#play");
  
-// function hungerLife(){
-//     for (i = 10, i > 0, i--)
 
-// }  
+// let hCounter=setInterval(hUpdated, 8000);
+//     let upto=10;
+//     function hUpdated(){
+//     const hCount= document.getElementById("hCounter");
+//     hCount.innerHTML=--upto;
+//     if(upto<=1){
+//     clearInterval(counts); 
+//     }
+// }
+
+// let eCounter=setInterval(eUpdated, 8000);
+
+//         function eUpdated(){
+//         const eCount= document.getElementById("eCounter");
+//         eCount.innerHTML=--upto;
+//         if(upto<=1){
+//         clearInterval(counts); 
+//     }
+// }
+// let pCounter=setInterval(pUpdated, 8000);
+
+//         function pUpdated(){
+//         const count= document.getElementById("pCounter");
+//         pCount.innerHTML=--upto;
+//         if(upto<=1){
+//         setTimeout(); 
+//     }
+// }
     
 //Add buttons to feed your pet, turn off the lights, and play with your pet
+const button = document.querySelector("js-button")
 
-    button.addEventListener("click", Feed());
+button.addEventListener("click", Feed());
     function Feed() {
     for(let h = 0; h >= 10; h++);{
        return (hunger[h] + 1) 
     }
 
-    }
-    button.addEventListener("click", Energy());
+}
+button.addEventListener("click", Energy());
 
     button.addEventListener("click", Play());
 
@@ -56,6 +82,6 @@ function lights() {
 
 //Add scores for each button
 
-    // Make buttons work
+
 
 // Pet dies if they reach zero, Pet changes as they age up
